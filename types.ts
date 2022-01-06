9064dfa5ac609581bb81a2c2e84ed719bae082c8
NAGARRO\swatantragoswami <swatantra.goswami_cs17@nagarro.com>
2022-01-06 20:40:27 +0530

    init

--- types.ts
+++ types.ts
@@ -22,4 +22,4 @@ export interface IProject {
   key_techs: string[];
 }
 
-export type Category = "react" | "node" | "express" | "blockchain" | "mongo";
+export type Category = "react" | "node" | "express" | "django" | "mongo";
